a9b46b975e853cd6f895cebb3ef9bad2ad0eaf81
Patryk Makowski <patryk.makowski23@gmail.com>
2022-12-19 17:45:43 +0100

    Finished part 1

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const incompleteTasks = document.getElementById("incomplete-tasks");
+const completedTasks = document.getElementById("completed-tasks");
 const addButton = document.getElementById("add-button");
+const editButton = document.getElementsByClassName("edit")[0];
 
 addButton.addEventListener("click", () => {
   const newTaskName = document.getElementById("new-task").value;
   createNewTask(newTaskName);
+});
+
+const allEditButtons = [...document.getElementsByClassName("edit")];
+allEditButtons.forEach((editButton) => {
+  editButton.addEventListener("click", () => {
+    const parent = editButton.parentNode;
+    const label = parent.querySelector("label");
+    const input = parent.querySelector("input[type=text]");
+    const isEditing = parent.classList.contains("editMode");
+
+    if (isEditing) {
+      label.innerText = input.value;
+    } else {
+      input.value = label.innerText;
+    }
+
+    parent.classList.toggle("editMode");
+  });
+});
+
+const allDeleteButtons = [...document.getElementsByClassName("delete")];
+allDeleteButtons.forEach((deleteButton) => {
+  deleteButton.addEventListener("click", () => {
+    const parent = deleteButton.parentNode;
+    parent.remove();
+  });
+});
+
+const allCheckBoxes = [...document.querySelectorAll("input[type=checkbox]")];
+allCheckBoxes.forEach((checkBox) => {
+  checkBox.addEventListener("click", () => {
+    const list = checkBox.parentNode.parentNode;
+    const listItem = checkBox.parentNode;
+    const isInCompleted = list.id === "completed-tasks";
+    if (isInCompleted) {
+      listItem.appendChild(editButton);
+      incompleteTasks.appendChild(listItem);
+    } else {
+      listItem.querySelector(".edit").remove();
+      completedTasks.appendChild(listItem);
+    }
+  });
 });
 
 const createNewTask = (newTaskName) => {
